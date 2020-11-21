a96f1d7adc3462d4fd0d4792d0aaf203a36d0654
Diego Arevalo <diego2000avelar@gmail.com>
2020-11-21 13:36:50 -0600

    changes

--- sheets.rb
+++ sheets.rb
@@ -4,19 +4,23 @@ require 'spreadsheet'
 workbook = Spreadsheet.open 'outputs.xls'
 
 def read_file sheet
+  puts sheet.name
   sheet.each do |row|
     puts "#{row[1]} - #{row[2]} - #{row[3]}"
   end
 end
 puts '----------Facil------------'
+
 # Specify a single worksheet by index => basic block
 sheet1 = workbook.worksheet 0
 read_file(sheet1)
 puts '----------Intermedio------------'
+
 # Specify a single worksheet by index => middle block 
 sheet2 = workbook.worksheet 1
 read_file(sheet2)
 puts '----------Dificil------------'
+
 # Specify a single worksheet by index => hard block 
 sheet3 = workbook.worksheet 2
 read_file(sheet3)
