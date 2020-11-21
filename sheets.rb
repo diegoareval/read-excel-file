3abf54e0f2e309d4380ef0319f0579cf174ee718
Diego Arevalo <diego2000avelar@gmail.com>
2020-11-20 22:40:02 -0600

    read file

--- sheets.rb
+++ sheets.rb
@@ -1,20 +1,27 @@
 require 'spreadsheet'
 
 # Open source spreadsheet
-workbook = Spreadsheet.open 'sheet1.xls'
+workbook = Spreadsheet.open 'outputs.xls'
 
-# Specify a single worksheet by index
+def read_file sheet
+  sheet.each do |row|
+    puts "#{row[1]} - #{row[2]} - #{row[3]}"
+  end
+end
+puts '----------Facil------------'
+# Specify a single worksheet by index => basic block
 sheet1 = workbook.worksheet 0
-sheet1.each do |row|
-  puts "#{row[0]} - #{row[1]} - #{row[2]}"
-end
-puts '----------------------' 
-sheet1 = workbook.worksheet 1
-sheet1.each do |row|
-  puts "#{row[0]} - #{row[1]} - #{row[2]}"
-end
+read_file(sheet1)
+puts '----------Intermedio------------'
+# Specify a single worksheet by index => middle block 
+sheet2 = workbook.worksheet 1
+read_file(sheet2)
+puts '----------Dificil------------'
+# Specify a single worksheet by index => hard block 
+sheet3 = workbook.worksheet 2
+read_file(sheet3)
 
-puts '----------------------'
+puts '----------Fin------------'
 
 # Loop over each worksheet
 #workbook.worksheets.each do |sheet|
